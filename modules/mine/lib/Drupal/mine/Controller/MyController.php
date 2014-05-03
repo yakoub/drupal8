630ff4339472714886973b755058aa2b6f62e73f
yakoub abaya <sting606@gmail.com>
2014-05-03 16:04:53 +0300

    using function controller instead of class

--- modules/mine/lib/Drupal/mine/Controller/MyController.php
+++ modules/mine/lib/Drupal/mine/Controller/MyController.php
@@ -4,33 +4,6 @@ namespace Drupal\mine\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class MyController {
-  public function main(Request $request) {
-    $build = array(
-      '#markup' => t('Welcome to mine'),
-    );
-
-    $lg = \Drupal::linkGenerator();
-    $items = array();
-    $args = array('type' => 'staff');
-    $items[] = $lg->generate(t('Staff'), 'mine.data', $args);
-
-    $args['type'] = 'departments';
-    $items[] = $lg->generate(t('Departments'), 'mine.data', $args);
-
-    $args['type'] = 'assignment';
-    $items[] = $lg->generate(t('Assignments'), 'mine.data', $args);
-    
-    $args['type'] = 'assignment';
-    $options['query'] = array('filter' => 'George');
-    $items[] = $lg->generate(t('George assignment'), 'mine.data', $args, $options);
-
-    $build['list'] = array(
-      '#theme' => 'item_list',
-      '#items' => $items, 
-    );
-
-    return $build;
-  }
 
   public function typeTitle($type) {
     $title = \Drupal::config('mine.demo')->get('titles.' . $type);
